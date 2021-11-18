65d904513132f34f4dc0299fb84f8905888c37fc
hotaosa <kfs.msl@gmail.com>
2021-11-18 19:04:06 +0900

    fix AGC049 B

--- AGC/AGC049/B/main.cc
+++ AGC/AGC049/B/main.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <map>
 
 int main() {
     int n;
@@ -8,46 +7,43 @@ int main() {
     std::string s, t;
     std::cin >> s >> t;
     if (s == t) { std::cout << "0" << std::endl; return 0; }
-    
-    std::map<std::string, std::vector<int> > pos;
+
+    std::vector<int> p_s, p_t;
     for (int i = 0; i < n; ++i) {
-        if (s[i] == '1') pos[s].push_back(i);
-        if (t[i] == '1') pos[t].push_back(i);        
+        if (s[i] == '1') p_s.push_back(i);
+        if (t[i] == '1') p_t.push_back(i);
     }
 
-    if (pos[s].size() < pos[t].size() || (pos[s].size() + pos[t].size()) & 1) {
+    if (p_s.size() < p_t.size() || (p_s.size() + p_t.size()) & 1) {
         std::cout << "-1" << std::endl;
         return 0;
     }
 
-    for (auto ps = pos[s].rbegin(), pt = pos[t].rbegin(); pt != pos[t].rend(); ++ps, ++pt) {
-        if (*ps < *pt) {
+    for (auto ritr_s = p_s.rbegin(), ritr_t = p_t.rbegin(); ritr_t != p_t.rend(); ++ritr_s, ++ritr_t) {
+        if (*ritr_s < *ritr_t) {
             std::cout << "-1" << std::endl;
             return 0;
         }
     }
 
-    int res = 0;
-    auto itr_s = pos[s].begin(), itr_t = pos[t].begin();
-    while (itr_t != pos[t].end()) {
+    long long res = 0;
+    auto itr_s = p_s.begin(), itr_t = p_t.begin();
+    while (itr_t != p_t.end()) {
         if (*itr_s >= *itr_t) {
             res += *itr_s - *itr_t;
             ++itr_s;
             ++itr_t;
         } else {
-            auto nx = itr_s;
-            ++nx;
-            res += *nx - *itr_s;
-            ++itr_s;
+            int cur = *itr_s;
+            int nx = *(++itr_s);
+            res += nx - cur;
             ++itr_s;
         }
     }
-
-    while (itr_s != pos[s].end()) {
-        auto nx = itr_s;
-        ++nx;
-        res += *nx - *itr_s;
-        ++itr_s;
+    while (itr_s != p_s.end()) {
+        int cur = *itr_s;
+        int nx = *(++itr_s);
+        res += nx - cur;
         ++itr_s;
     }
 
