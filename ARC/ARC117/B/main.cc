f3c8ca1a33655dc7c7e2b16fdc6dc4d8674d2db5
hotaosa <kfs.msl@gmail.com>
2021-11-29 22:24:32 +0900

    fix ARC117 B

--- ARC/ARC117/B/main.cc
+++ ARC/ARC117/B/main.cc
@@ -3,16 +3,19 @@
 #include <algorithm>
 
 int main() {
-  const long long MOD = 1000000007LL;
+  constexpr long long MOD = 1000000007LL;
   int n;
   std::cin >> n;
+
   std::vector<long long> a(n);
-  for (int i = 0; i < n; ++i) std::cin >> a[i];
+  for (auto &p : a) std::cin >> p;
   std::sort(a.begin(), a.end());
+
   long long res = a[0] + 1LL;
   for (int i = 1; i < n; ++i) {
     res = res * (a[i] - a[i - 1] + 1LL) % MOD;
   }
+  
   std::cout << res << std::endl;
   return 0;  
 }
