17f2a86e14b8f4e30513e47894fac2bee88af70c
hotaosa <kfs.msl@gmail.com>
2022-03-16 12:35:13 +0900

    add ABC156 E

---
+++ ABC/ABC156/E/main.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using mint = atcoder::modint1000000007;
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  const int SIZE = 2 * n + 1;
+
+  vector<mint> fact(SIZE), finv(SIZE);
+  fact[0] = fact[1] = finv[0] = finv[1] = 1;
+  for (int i = 2; i < SIZE; ++i) fact[i] = fact[i - 1] * i;
+  finv[SIZE - 1] = fact[SIZE - 1].inv();
+  for (int i = SIZE - 1; i > 2; --i) finv[i - 1] = finv[i] * i;
+  
+  auto binom = [&](int n, int r) -> mint {
+    if (n < 0 || r < 0 || n < r) return 0;
+    return fact[n] * finv[r] * finv[n - r];
+  };
+  auto multi = [&](int n, int r) -> mint {
+    return binom(n + r - 1, r);
+  };
+
+  mint res = 0;
+  for (int i = 0; i <= min(k, n - 1); ++i) res += binom(n, i) * multi(n - i, i);
+  cout << res.val() << endl;
+  return 0;
+}
