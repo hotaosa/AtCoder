10e2bc09bc5ebf076062d81f9508f0b67c1ce343
hotaosa <kfs.msl@gmail.com>
2025-02-09 14:42:24 +0900

    modify ABC392/G

--- ABC/ABC392/G/main.cc
+++ ABC/ABC392/G/main.cc
@@ -1,25 +1,23 @@
 #include <iostream>
+#include <numeric>
 #include <vector>
 //
 #include <atcoder/convolution>
 
 int main() {
-  constexpr int SIZE = 1000000;
   int n;
   std::cin >> n;
-  std::vector<int> p(SIZE, 0);
-  for (int i = 0; i < n; ++i) {
-    int a;
-    std::cin >> a;
-    p[--a] = 1;
-  }
+  std::vector<int> s(n);
+  for (auto &e : s) std::cin >> e;
 
+  const std::size_t siz = *max_element(s.begin(), s.end()) + 1;
+  std::vector<int> p(siz, 0);
+  for (int x : s) p[x] = 1;
+
+  const auto conv = atcoder::convolution(p, p);
   long long res = 0;
-  auto conv = atcoder::convolution(p, p);
-  for (int i = 0; i < SIZE; ++i) {
-    if (p[i]) {
-      res += (conv[i * 2] - 1) / 2;
-    }
+  for (std::size_t i = 0; i < siz; ++i) {
+    if (p[i]) res += (conv[i * 2] - 1) >> 1;
   }
 
   std::cout << res << "\n";
